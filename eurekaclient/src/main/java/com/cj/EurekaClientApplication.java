d4e46c9201a16c81f5a4260275be713554c5e5ba
wb-cj189958 <wb-cj189958@alibaba-inc.com>
2018-05-31 11:24:45 +0800

    add

---
+++ eurekaclient/src/main/java/com/cj/EurekaClientApplication.java
@@ -0,0 +1,39 @@
+package com.cj;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author wb-cj189958
+ * @date 2018/5/26 20:56
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/{applicationName}")
+    public List<ServiceInstance> serviceInstance(
+            @PathVariable String applicationName) {
+        return discoveryClient.getInstances(applicationName);
+    }
+}
---
+++ eurekaserver/src/main/java/com/cj/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.cj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wb-cj189958
+ * @date 2018/5/26 20:12
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
